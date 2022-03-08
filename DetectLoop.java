1802895c6fc292a22410d5f1be054b6dc9507d11
PRIYANSHU SHARMA <98525774+PriyanshuSharma2@users.noreply.github.com>
2022-03-08 22:29:39 +0530

    Create DetectLoop.java

---
+++ DetectLoop.java
@@ -0,0 +1,14 @@
+ public static boolean detectLoop(Node head){
+        // Add code here
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        ret
