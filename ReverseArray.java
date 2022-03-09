22832ae94a271993eaa170334657d76b3180a4c9
PRIYANSHU SHARMA <98525774+PriyanshuSharma2@users.noreply.github.com>
2022-03-09 19:53:34 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -2,20 +2,33 @@ import java.util.Scanner;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         System.out.println("Enter the size of an array: ");
         int size = sc.nextInt();
-        System.out.println("Enter the element: ");
-        int [] arr = new int[size];
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of an array");
         for (int i = 0; i <arr.length ; i++) {
-            arr[i]= sc.nextInt();
+            arr[i] = sc.nextInt();
         }
-        int count =0;
-        for (int i = 0; i < arr.length-1; i++) {
-            if(arr[i]>=arr[i+1])
-            {
-
-            }
+        System.out.println("Your array you have enter");
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\nYour array after reverse");
+        int start = 0;
+        int end = arr.length-1;
+        int temp = 0;
+        while(start<end)
+        {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start ++;
+            end --;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
         }
     }
 }
+
