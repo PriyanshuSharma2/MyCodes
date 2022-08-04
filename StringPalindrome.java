72b2f90e9a29120c8478c7a8a6d012d4d63a9692
PRIYANSHU SHARMA <98525774+PriyanshuSharma2@users.noreply.github.com>
2022-08-04 21:51:26 +0530

    Create StringPalindrome.java

---
+++ StringPalindrome.java
@@ -0,0 +1,16 @@
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String S) {
+        // code here
+        int i = 0;
+        int j = S.length()-1;
+        while(i<j){
+            if(S.charAt(i)!=S.charAt(j)) return 0;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+};
