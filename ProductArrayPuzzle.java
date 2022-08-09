edbfb32fe0ae651b8a8b51932f9552201eddda41
PRIYANSHU SHARMA <98525774+PriyanshuSharma2@users.noreply.github.com>
2022-08-09 11:34:38 +0530

    Create ProductArrayPuzzle.java

---
+++ ProductArrayPuzzle.java
@@ -0,0 +1,26 @@
+class Solution 
+{ 
+	public static long[] productExceptSelf(int nums[], int n) 
+	{ 
+        // code here
+        long[] left = new long[n];
+        long[] right = new long[n];
+        long[] num = new long[n];
+        left[0] = 1;
+        right[n-1] = 1;
+        for(int i = 1; i<nums.length; i++)
+        {
+             left[i] = left[i-1]*nums[i-1];
+        }
+        for(int i = n-2; i>=0; i--)
+        {
+             
+             right[i]=right[i+1]*nums[i+1];
+        }
+        for(int i = 0;i<nums.length;i++)
+        {
+            num[i] = left[i]*right[i];
+        }
+        return num;
+	} 
+} 
