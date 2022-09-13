662f753e8427209369ef23ea519a6736dd636830
PRIYANSHU SHARMA <98525774+PriyanshuSharma2@users.noreply.github.com>
2022-09-13 22:36:22 +0530

    Create ConatinsduplicateII.java

---
+++ ConatinsduplicateII.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i = 0; i<nums.length; i++){
+            if(!map.containsKey(nums[i])) map.put(nums[i],i);
+            else {
+                if(Math.abs(map.get(nums[i])-i)<=k) return true;
+                else map.put(nums[i],i);
+            }
+    }
+         return false;
+  }
+}
